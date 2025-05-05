9677929c82092f5c3087c1a359a3c51f0c6141b8
patrick-moling-unitn <patrick.moling@studenti.unitn.it>
2025-05-05 20:18:29 +0200

    Creata la configurazione di base del progetto VUE con l'index e senza il mainJS

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express")
+const CORS = require("cors")
+
 const app = express()
 
+app.use(CORS())
+
+const path = __dirname + '/app/';
+console.log(path)
+app.use(express.static(path));
+
 app.use("/", (req, res) =>{
-    res.send("Test")
+    res.sendFile(path + "index.html");
 })
 
 app.listen(3000, () => { console.log("Listening on port 3000") })
