25ad695ddb13a5316e6f5a36738a60447c9a984b
patrick-moling-unitn <patrick.moling@studenti.unitn.it>
2025-05-07 20:05:04 +0200

    Aggiunta il backend per il fetch e la visualizzazione dei cestini. Aggiunto un semplice POST di testing (temporaneo) per popolare il DB.

--- app/src/components/OpenStreethMap.vue
+++ app/src/components/OpenStreethMap.vue
@@ -8,12 +8,14 @@ export default {
       //......data of your component
     }),
     mounted() {
-        let geolocalizedPosition = new L.LatLng(0,0);
+        let geolocalizedPosition = L.latLng(0,0);
         let geolocalizationSuccess = false;
         let geolocalizationMarket;
         let repeater;
 
-        const PORT = process.env.SERVER_PORT;
+        //const PORT = process.env.SERVER_PORT;
+
+        let trashcanMarkers = [];
 
         getLocation();
 
@@ -33,12 +35,7 @@ export default {
           var center = map.getCenter();
           console.log("Map center coordinates:", center.lat, center.lng);
           
-          fetch(`http://localhost:${3000}/trashcans`)
-            .then(response => response.json())
-            .then(trashcans => { 
-              console.log(trashcans)
-              //Show the trashcans on the map
-            });
+          showAllTrashcans();
         });
 
         function getLocation() {
@@ -52,10 +49,11 @@ export default {
         function success(position) {
           console.log("Latitude: " + position.coords.latitude +
           " Longitude: " + position.coords.longitude);
-          geolocalizedPosition = new L.LatLng(position.coords.
+          geolocalizedPosition = L.latLng(position.coords.
             latitude, position.coords.longitude);
 
           if (!geolocalizationSuccess){
+            addNewTrashcan(); //---TEST--- aggiunge un cestino vicino la posizione attuale
             geolocalizationSuccess = true;
             map.panTo(geolocalizedPosition);
           }
@@ -72,6 +70,55 @@ export default {
             geolocalizationMarket = L.marker(geolocalizedPosition).addTo(map);
           else
             geolocalizationMarket.setLatLng(geolocalizedPosition)
+        }
+
+        function showAllTrashcans(){
+          fetch(`http://localhost:${3000}/trashcans`)
+            .then(response => response.json())
+            .then(trashcans => { 
+              console.log(trashcans)
+              
+              let i = 0;
+              trashcans.forEach(element => {
+                let lat = parseFloat(element.latitude.$numberDecimal);
+                let lng = parseFloat(element.longitude.$numberDecimal);
+                console.log(trashcanMarkers);
+                console.log(trashcanMarkers.length);
+
+                let position = L.latLng(lat, lng);
+                if (trashcanMarkers.length > i){
+                  let marker = trashcanMarkers[i];
+                  marker.setLatLng(position);
+                }
+                else{
+                  let marker = L.marker(position);
+                  trashcanMarkers.push(marker);
+                  marker.addTo(map);
+                }
+                i++;
+              });
+
+              if (trashcans.length < trashcanMarkers.length){
+                for (i=trashcans.length; i<trashcanMarkers.length; i++){
+                  trashcanMarkers[i].removeFrom(map);
+                }
+                trashcanMarkers = trashcanMarkers.slice(trashcans.length);
+              }
+            });
+        }
+
+        function addNewTrashcan(){
+          fetch(`http://localhost:${3000}/trashcans`, {
+            method: "POST",
+            body: JSON.stringify({
+              latitude: geolocalizedPosition.lat + (Math.random() - Math.random())/100,
+              longitude: geolocalizedPosition.lng + (Math.random() - Math.random())/100,
+              trashcanType: 3
+            }),
+            headers: {
+              "Content-type": "application/json; charset=UTF-8"
+            }
+          });
         }
     },
     methods: {
