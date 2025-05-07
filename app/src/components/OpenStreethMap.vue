f540be630c1d02cccfa8e777d9b3b9e651fae07c
Patrick <patrick.moling@studenti.unitn.it>
2025-05-07 09:54:37 +0200

    Aggiunta la funzionalità di geolocalizzazione della mappa

--- app/src/components/OpenStreethMap.vue
+++ app/src/components/OpenStreethMap.vue
@@ -3,17 +3,19 @@
 </template>
 
 <script template>
+import vector2 from "vector2"
 export default {
     data: () => ({
       //......data of your component
     }),
     mounted() {
-        let mapDiv = document.createElement('div')
-        mapDiv.setAttribute('id','map')
-        document.body.appendChild(mapDiv)
-      
+        let geolocalizedPosition = new L.LatLng(0,0);
+        getLocation();
+
+        console.log("creating map options")
+
         let mapOptions = {
-            center:[51.958, 9.141],
+            center:[geolocalizedPosition.lat, geolocalizedPosition.lng],
             zoom:100
         }
 
@@ -21,6 +23,26 @@ export default {
 
         let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
         map.addLayer(layer);
+
+        function getLocation() {
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, error);
+          } else {
+            alert("Geolocation is not supported by this browser.");
+          }
+        }
+
+        function success(position) {
+          console.log("Latitude: " + position.coords.latitude +
+          " Longitude: " + position.coords.longitude);
+          geolocalizedPosition = new L.LatLng(position.coords.
+            latitude, position.coords.longitude);
+          map.panTo(geolocalizedPosition)
+        }
+
+        function error() {
+          alert("Sorry, no position available.")
+        }
     },
     methods: {
       //......methods of your component
