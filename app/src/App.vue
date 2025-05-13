4be6ac856f9c99da45d9a8e998ea14719fbe196f
Patrick <patrick.moling@studenti.unitn.it>
2025-05-13 12:47:21 +0200

    Aggiunto il ruoting fra i componenti del VueJS

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,57 +1,30 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import OpenStreethMap from './components/OpenStreethMap.vue'
+import { ref, computed } from 'vue'
 import Register from './components/Register.vue'
+import EmailVerification from './components/EmailVerification.vue'
+import OpenStreethMap from './components/OpenStreethMap.vue'
+//import NotFound from './NotFound.vue'
+
+const routes = {
+  '/': OpenStreethMap,
+  '/register': Register,
+  '/registration/verify': EmailVerification,
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header> -->
-
-  <main>
-    <section> <!-- sezione mappa -->
-      <OpenStreethMap/>
-    </section>
-    <section> <!-- sezione registrazione -->
-      <Register/>
-    </section>
-    <section>
-      <TheWelcome />
-    </section>
-  </main>
+  <a href="#/">Home</a> |
+  <a href="#/register">Register</a> |
+  <a href="#/registration/verify">Verify email</a>
+  <component :is="currentView" />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
