3cee938c9e663137804d4a81647ab2adf4ae7755
Raffaele-cell <raffaele.cella@studenti.unitn.it>
2025-05-17 17:27:20 +0200

    Push della funzionalità di ricerca cestino più vicino.

--- api/trashcanManager.js
+++ api/trashcanManager.js
@@ -57,4 +57,45 @@ router.delete('/:id', async (req, res) => {
     res.status(204).send()
 });
 
+router.get("/:typeAndPosition", async (req, res) => {
+    let userLat = parseFloat(req.params.position.latitude.$numberDecimal);
+    let userLng = parseFloat(req.params.position.longitude.$numberDecimal);
+
+
+    let smallestTheta = 4.0; //Larger than PI
+    let nearestTrashcan;
+    trashcanList.forEach(element => {
+        if (element.trashcanType == req.params.type) {
+            let lat = parseFloat(element.latitude.$numberDecimal);
+            let lng = parseFloat(element.longitude.$numberDecimal);
+        
+            let dLng = userLng - lng;
+
+            let y = Math.sin(dLng) * Math.cos(lat2);
+            let x = Math.cos(userLat) * Math.sin(lat) - Math.sin(userLat) * Math.cos(lat) * Math.cos(dLng);
+
+            let theta = Math.atan2(y, x);
+            theta += 2 * Math.PI;
+            theta %= 2 * Math.PI;
+            theta = 2 * Math.PI - theta;
+
+            if (theta < smallestTheta)
+            {
+                nearestTrashcan = element;
+                smallestTheta = theta;
+            }
+        }
+    });
+
+    if (smallestTheta == 4.0) {
+        console.log("No trashcan found.");
+        res.status(404);
+    }
+    else {
+        console.log("Nearest trashcan: "+nearestTrashcan)
+        res.status(200).json(nearestTrashcan);
+    }
+})
+
+
 module.exports = router;
